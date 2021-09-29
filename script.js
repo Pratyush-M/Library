50337007e8659d4b42bf24c53244b29555691ac6
Pratyush Mendon <mendonpratyush908@gmail.com>
2021-09-29 22:37:48 +0530

    add css to center div, add spans to html to test alignment, add function to append new book as a node

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-let myLibrary = [];
+let myLibrary = [
+    
+       { title: 'Four Agreements',
+        author: 'Don Miguel Ruiz',
+        pages: 93,
+        read: 'Read', },
+
+        { title: 'Eloquent Javascript',
+        author: 'Marijne Haverbeke',
+        pages: 500,
+        read: 'Unread', }
+];
 
 function Book(title, author, pages, read) {
  this.title = title;
@@ -11,10 +22,23 @@ function Book(title, author, pages, read) {
 function addBookToLibrary() {
 let title = prompt('Name of book');
 let author = prompt('Name of Author');
+let pages = prompt('Number of pages')
+let read = prompt('Book status (Read/Unread)')
 
-let bookObject = new Book(title, author);
+let bookObject = new Book(title, author, pages, read);
 myLibrary.push(bookObject);
 };
 
-addBookToLibrary();
+function showBooks () {
+   let grid = document.getElementsByClassName("container")[0];
+    let span = document.createElement('span');
+    span.textContent = 'hey';
+    grid.appendChild(span);
+    
+    
+}
+
+ showBooks();
+
+// addBookToLibrary();
 console.log(myLibrary)
