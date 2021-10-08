afbe3a6c521f1a5078e4ace5c45663126c68e4a9
Pratyush Mendon <mendonpratyush908@gmail.com>
2021-10-08 07:16:13 +0530

    add a function expression libraryLoop in attempt to only add last object from array

--- script.js
+++ script.js
@@ -45,7 +45,11 @@ let evaluator = function (e) {
          document.querySelector('form').classList.remove('showForm')
          
          document.querySelector('form').reset();
-
+        
+         let grid = document.getElementsByClassName("container")[0];
+         let li = document.createElement('li');
+         
+             
      } 
 }
 document.getElementById("submit").onclick = evaluator;
@@ -59,50 +63,49 @@ document.getElementById('addBook').addEventListener('click', () => {document.que
 function showBooks () {
     let grid = document.getElementsByClassName("container")[0];
     let li = document.createElement('li');
+
+    let libraryLoop = function (element, indexMain) {
+        let span = document.createElement('span');
     
-        myLibrary.forEach( function (element, indexMain) {
-            let span = document.createElement('span');
-
-            Object.keys(myLibrary[indexMain]).forEach( function (key, index) {
-
-                if(key == 'author') {
-                li = document.createElement('li');
-                li.textContent = 'By ' + myLibrary[indexMain][key] ;
-                grid.appendChild(span).appendChild(li);
-                }
-
-                else if(key == 'pages') {
-                li = document.createElement('li');
-                li.textContent =  myLibrary[indexMain][key] + ' Pages' ;
-                grid.appendChild(span).appendChild(li);
-                }
-
-                else if(myLibrary[indexMain][key] == 'Yes') {
-                li = document.createElement('li');
-                li.textContent = "Read" ;
-                grid.appendChild(span).appendChild(li);
-                }
-
-                else if (myLibrary[indexMain][key] == 'No'){
-                li = document.createElement('li');
-                li.textContent = "Unread" ;
-                grid.appendChild(span).appendChild(li);
-                }
-
-                else {
-                li = document.createElement('li');
-                li.textContent = myLibrary[indexMain][key];
-                grid.appendChild(span).appendChild(li);
-                }
-
-                
-            })
-            let removeBtn = document.createElement('button');
-                removeBtn.textContent = "Remove"
-                span.appendChild(removeBtn);
+        Object.keys(myLibrary[indexMain]).forEach( function (key, index) {
     
+            if(key == 'author') {
+            li = document.createElement('li');
+            li.textContent = 'By ' + myLibrary[indexMain][key] ;
+            grid.appendChild(span).appendChild(li);
+            }
+    
+            else if(key == 'pages') {
+            li = document.createElement('li');
+            li.textContent =  myLibrary[indexMain][key] + ' Pages' ;
+            grid.appendChild(span).appendChild(li);
+            }
+    
+            else if(myLibrary[indexMain][key] == 'Yes') {
+            li = document.createElement('li');
+            li.textContent = "Read" ;
+            grid.appendChild(span).appendChild(li);
+            }
+    
+            else if (myLibrary[indexMain][key] == 'No'){
+            li = document.createElement('li');
+            li.textContent = "Unread" ;
+            grid.appendChild(span).appendChild(li);
+            }
+    
+            else {
+            li = document.createElement('li');
+            li.textContent = myLibrary[indexMain][key];
+            grid.appendChild(span).appendChild(li);
+            }
+    
+            
         })
-    
+        let removeBtn = document.createElement('button');
+            removeBtn.textContent = "Remove"
+            span.appendChild(removeBtn);
+    }
+        myLibrary.forEach(libraryLoop)
 }
 
 showBooks()
@@ -110,4 +113,3 @@ showBooks()
 console.log(myLibrary) ;
 
 document.getElementsByClassName('close')[0].addEventListener('click', () => document.querySelector('form').classList.remove('showForm'))
-
